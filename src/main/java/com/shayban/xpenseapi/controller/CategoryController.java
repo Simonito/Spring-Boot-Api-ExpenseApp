bd6ce3d1ed2522f28f9691932953dcff5b170fe8
Šimon Ukuš <xukus@stuba.sk>
2024-03-08 13:18:37 +0100

    Implement POST PUT DELETE in category and expense
    Signed-off-by: Šimon Ukuš <xukus@stuba.sk>

--- src/main/java/com/shayban/xpenseapi/controller/CategoryController.java
+++ src/main/java/com/shayban/xpenseapi/controller/CategoryController.java
@@ -1,10 +1,15 @@
 package com.shayban.xpenseapi.controller;
 
 import com.shayban.xpenseapi.model.Category;
+import com.shayban.xpenseapi.model.Expense;
 import com.shayban.xpenseapi.repository.CategoryRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -29,7 +34,27 @@ public class CategoryController {
     @GetMapping("/{id}")
     public Category getAll(@PathVariable("id") UUID id) {
         return this.repo
-                .findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found"));
+            .findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found"));
+    }
+
+
+    @PostMapping
+    public Category add(@RequestBody Category category) {
+        // TODO: add verification and exception handling
+        return repo.save(category);
+    }
+
+    @PutMapping("/{id}")
+    public Category update(@PathVariable UUID id, @RequestBody Category category) {
+        repo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found"));
+        return repo.save(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable UUID id) {
+        final Category toDel = repo.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found"));
+        repo.delete(toDel);
     }
 }
--- src/main/java/com/shayban/xpenseapi/controller/ExpenseController.java
+++ src/main/java/com/shayban/xpenseapi/controller/ExpenseController.java
@@ -3,8 +3,12 @@ package com.shayban.xpenseapi.controller;
 import com.shayban.xpenseapi.model.Expense;
 import com.shayban.xpenseapi.repository.ExpenseRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -29,7 +33,26 @@ public class ExpenseController {
     @GetMapping("/{id}")
     public Expense getAll(@PathVariable("id") UUID id) {
         return this.repo
-                .findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "expense not found"));
+            .findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "expense not found"));
+    }
+
+    @PostMapping
+    public Expense add(@RequestBody Expense expense) {
+        // TODO: add verification and exception handling
+        return repo.save(expense);
+    }
+
+    @PutMapping("/{id}")
+    public Expense update(@PathVariable UUID id, @RequestBody Expense expense) {
+        repo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "expense not found"));
+        return repo.save(expense);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable UUID id) {
+        final Expense toDel = repo.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "expense not found"));
+        repo.delete(toDel);
     }
 }
